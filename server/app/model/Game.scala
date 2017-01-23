3ad9485a787cb18373bb326d7a41b69901f3d2e5
Sabrina Friedl <Sabrina.Friedl@campus.lmu.de>
2017-01-23 16:35:48 +0100

    Adds model/Game

--- server/app/model/Game.scala
+++ server/app/model/Game.scala
@@ -1,4 +1,24 @@
-/**
-  * Created by sabrina on 23/01/2017.
-  */
+package model
+
+trait Game[T]{
+  def update(action: Action[T]): Status
+  def availableActions(): List[Action[T]]
+}
+
+case class Action[T](data: T)
+class Status{
+  val playerLabel: Char = ' '
+}
+
+class TicTacToe extends Game[(Int, Int)]{
+  override def update(action: Action[(Int, Int)]): Status = {
+    ???
+  }
+  override def availableActions() = {
+    ???
+  }
+  val field: Array[Array[Char]]  = Array.ofDim(3, 3)
+}
+
+
 
