90081448a1dfdc44d4a7bcf8f879db3694d8885c
Sabrina Friedl <Sabrina.Friedl@campus.lmu.de>
2017-02-01 22:32:14 +0100

    Adds refined Game implementation, Cake Pattern now works

--- server/app/model/Game.scala
+++ server/app/model/Game.scala
@@ -1,198 +1,163 @@
+/**
+  * Created by sabrina on 01/02/2017.
+  */
+
 package model
 
 trait Game[T] {
-  var state: State
-  var player: Player
-  var field: Array[Array[Char]]
-  def getAvailableActions(): List[Action[T]]
-  def update(action: Action[T]): State
+  this : Field with Update[T] =>
+  var state: State = new State
+  var player: Player = new Player
+  var field = this.playField //this = Field
+
+  def getAvailableActions(): List[Action[T]] = this.availableActions  //this = Update
+  def updateGameState(action: Action[T]): State = {
+    state = this.update(action, field, player, state)     //this = Update
+    if (state.gameState == "ongoing"){
+      player.switch()
+    }
+    else if (state.gameState == "won"){
+      println("Game over. Player " + player.playerLabel + " has won." )
+    }
+    else{
+      println("Game over. Draw!")
+    }
+    return(state)
+  }
 }
 
-trait TicTacToe extends Game[Location]{
-  override var state = new State() //ongoing
-  override var player = new Player()
-  override var field = Array.fill(3, 3)(' ')
-  private var availableActions = (for (i <- 0 to 2; j <- 0 to 2) yield (Action(Location(i, j)))).toList
-  override def getAvailableActions(): List[Action[Location]] = availableActions
 
-  override def update(action: Action[Location]): State = {
+trait Field{
+  def playField: Array[Array[Char]]
+}
+
+trait FieldTicTacToe extends Field{
+  override def playField: Array[Array[Char]] = Array.fill(3, 3)('_')
+}
+
+trait FieldFourWins extends Field{
+  override def playField: Array[Array[Char]] = Array.fill(6, 7)('_')
+}
+
+
+trait Update[T]{
+  var availableActions: List[Action[T]]
+  def update(action: Action[T], f: Array[Array[Char]], p:Player, s:State): State
+  def checkWin(lastRow: Int, lastCol: Int, maxRow: Int, maxCol: Int, noRequired: Int, f:Array[Array[Char]]): String ={
+    var gameState = "ongoing"
+    val curPlayer = f(lastRow)(lastCol)
+    var inLine = 1
+    val min = -(noRequired-1)
+    val max = noRequired-2
+
+    //check row
+    for(i <- min to max){
+      if(lastCol+i >= 0 && lastCol+i+1 <=maxCol){
+        if (f(lastRow)(lastCol+i) == curPlayer && f(lastRow)(lastCol+i+1) == curPlayer) {
+          inLine += 1
+        }
+      }
+    }
+    if (inLine >=noRequired){
+      gameState = "won"
+      return(gameState)
+    }
+
+    //check column
+    inLine = 1
+    for(i <- min to max){
+      if(lastRow+i >= 0 && lastRow+i+1 <=maxRow){
+        if (f(lastRow+i)(lastCol) == curPlayer && f(lastRow+i+1)(lastCol) == curPlayer){
+          inLine +=1
+        }
+      }
+    }
+    if (inLine >= noRequired){
+      gameState = "won"
+      return(gameState)
+    }
+
+    //check left bottom to right top
+    inLine = 1
+    for(i <- min to max){
+      if(lastRow+i >= 0 &&lastCol+i >= 0 && lastRow+i+1 <= maxRow && lastCol+i+1 <= maxCol){
+        if (f(lastRow+i)(lastCol+i) == curPlayer && f(lastRow+i+1)(lastCol+i+1) == curPlayer){
+          inLine +=1
+        }
+      }
+    }
+    if (inLine >=noRequired){
+      gameState = "won"
+      return(gameState)
+    }
+
+    //check left top to right bottom
+    inLine = 1
+    for(i <- min to max){
+      if(lastRow+i >= 0 && lastCol-i-1 >= 0 && lastRow+i+1 <=maxRow &&lastCol-i <= maxCol) {
+        if (f(lastRow+i)(lastCol-i) == curPlayer && f(lastRow+i+1)(lastCol-i-1) == curPlayer) {
+          inLine += 1
+        }
+      }
+    }
+    if (inLine >=noRequired){
+      gameState = "won"
+      return(gameState)
+    }
+
+    //no winner or loser
+    if (availableActions.isEmpty) {
+      gameState = "even"
+      return(gameState)
+    }
+
+    else {
+      gameState = "ongoing"
+      return(gameState)
+    }
+  }
+}
+
+trait UpdateTicTacToe extends Update[Location]{
+  override var availableActions = (for (i <- 0 to 2; j <- 0 to 2) yield (Action(Location(i, j)))).toList
+  override def update(action: Action[Location], f: Array[Array[Char]], p: Player, s:State): State = {
     if (availableActions.contains(action)) {
       val r = action.data.row
       val c = action.data.col
-      field(r)(c) = this.player.playerLabel
+      f(r)(c) = p.playerLabel
       availableActions = availableActions.filter(_ != action)
-      updateState(r, c)
+      s.gameState = checkWin(r, c, f)
+      return s
     }
+
     else{
       println("Field not available, please try again!")
-      return(state)
+      return s
     }
   }
-  def updateState(lastRow: Int, lastCol: Int): State = {
-    val curPlayer = this.player.playerLabel
-    var inLine = 1
-    //check row
-    for(i <- -2 to 1){
-      if(lastCol+i >= 0 && lastCol+i+1 <=2){
-        if (field(lastRow)(lastCol+i) == curPlayer && field(lastRow)(lastCol+i+1) == curPlayer) {
-          inLine += 1
-        }
-      }
-    }
-    if (inLine >=3){
-      state.gameState = "won"
-      return(state)
-    }
-
-    //check column
-    inLine = 1
-    for(i <- -2 to 1){
-      if(lastRow+i >= 0 && lastRow+i+1 <=2){
-        if (field(lastRow+i)(lastCol) == curPlayer && field(lastRow+i+1)(lastCol) == curPlayer){
-          inLine +=1
-        }
-      }
-    }
-    if (inLine >=3){
-      state.gameState = "won"
-      return(state)
-    }
-
-    //check left bottom to right top
-    inLine = 1
-    for(i <- -2 to 1){
-      if(lastRow+i >= 0 &&lastCol+i >= 0 && lastRow+i+1 <=2 && lastCol+i+1 <=2){
-        if (field(lastRow+i)(lastCol+i) == curPlayer && field(lastRow+i+1)(lastCol+i+1) == curPlayer){
-          inLine +=1
-        }
-      }
-    }
-    if (inLine >=3){
-      state.gameState = "won"
-      return(state)
-    }
-
-    //check left top to right bottom
-    inLine = 1
-    for(i <- -2 to 1){
-      if(lastRow+i >= 0 && lastCol-i-1 >= 0 && lastRow+i+1 <=2 &&lastCol-i <= 2) {
-        if (field(lastRow+i)(lastCol-i) == curPlayer && field(lastRow+i+1)(lastCol-i-1) == curPlayer) {
-          inLine += 1
-        }
-      }
-    }
-    if (inLine >=3){
-      state.gameState = "won"
-      return(state)
-    }
-
-    //no winner or loser
-    if (availableActions.isEmpty) {
-      state.gameState = "even"
-      return(state)
-    }
-
-    else {
-      state.gameState = "ongoing"
-      player.switch() //switch player
-      return(state)
-    }
-  }
+  def checkWin = super.checkWin(_:Int, _: Int, 2, 2, 3, _:Array[Array[Char]])
 }
 
 
-
-
-trait FourWins extends Game[Column]{
-  override var state = new State() //ongoing
-  override var player = new Player()
-  override var field = Array.fill(6, 7)(' ') //0,0 is bottom left!
+trait UpdateFourWins extends Update[Column]{
   private var curRow = Array.fill(7)(0) //per column, remember last free row
-
-  private var availableActions = (for (c <- 0 to 6) yield (Action(Column(c)))).toList
-  override def getAvailableActions(): List[Action[Column]] = availableActions
-
-  override def update(action: Action[Column]): State = {
+  override var availableActions = (for (c <- 0 to 6) yield (Action(Column(c)))).toList
+  override def update(action: Action[Column], f: Array[Array[Char]], p: Player, s: State): State = {
     if (availableActions.contains(action)) {
       val c = action.data.col
-      field(curRow(c))(c) = this.player.playerLabel
+      f(curRow(c))(c) = p.playerLabel
       if (curRow(c) >= 5){
         availableActions = availableActions.filter(_ != action) //if column is full, remove from available actions
       }
-      updateState(curRow(c), c)
-      curRow(c) += 1
-      return(state)
+      s.gameState = checkWin(curRow(c), c, f)
+      return s
     }
     else{
-      printf("Column already full, please try again!")
-      return(state)
+      println("Column already full, please try again!")
+      return s
     }
   }
-
-  def updateState(lastRow: Int, lastCol:Int): State = {
-    val curPlayer = this.player.playerLabel
-    var inLine = 1
-
-    //check row
-    for(i <- -3 to 2){
-      if(lastCol+i >= 0 && lastCol+i <=6)
-      if (field(lastRow)(lastCol+i) == curPlayer && field(lastRow)(lastCol+i+1) == curPlayer){
-        inLine +=1
-      }
-    }
-    if (inLine >=4){
-      state.gameState = "won"
-      return(state)
-    }
-
-    //check column
-    inLine = 1
-    for(i <- -3 to 2){
-      if (field(lastRow+i)(lastCol) == curPlayer && field(lastRow+i+1)(lastCol) == curPlayer){
-        inLine +=1
-      }
-    }
-    if (inLine >=4){
-      state.gameState = "won"
-      return(state)
-    }
-
-    //check left bottom to right top
-    inLine = 1
-    for(i <- -3 to 2){
-      if (field(lastRow+i)(lastCol+i) == curPlayer && field(lastRow+i+1)(lastCol+i) == curPlayer){
-        inLine +=1
-      }
-    }
-    if (inLine >=4){
-      state.gameState = "won"
-      return(state)
-    }
-
-    //check left top to right bottom
-    inLine = 1
-    for(i <- -3 to 2){
-      if (field(lastRow+i)(lastCol-i) == curPlayer && field(lastRow+i+1)(lastCol-i-1) == curPlayer){
-        inLine +=1
-      }
-    }
-    if (inLine >=4){
-      state.gameState = "won"
-      return(state)
-    }
-
-    //no winner or loser
-    if (availableActions.isEmpty) {
-      state.gameState = "even"
-      return(state)
-    }
-
-    else {
-      state.gameState = "ongoing"
-      player.switch() //switch player
-      return(state)
-    }
-  }
+  def checkWin = super.checkWin(_:Int, _: Int, 5, 6, 4, _:Array[Array[Char]])
 }
 
 
@@ -219,61 +184,61 @@ case class Location(r: Int, c: Int){
   val col: Int = c
 }
 
-case class Column( c: Int){
+case class Column(c: Int){
   val col: Int = c
 }
 
 
-
 object main extends App{
-  var newGame = new TicTacToe {}
-  println("player: " + newGame.player.playerLabel)
-  newGame.update(Action(Location(1,1))) //X
-  println("field: " + newGame.field(1)(1))
-  println("state: " + newGame.state.gameState)
-  println("new player: " + newGame.player.playerLabel)
-  newGame.update(Action(Location(2, 0))) //O
-  println("state: " + newGame.state.gameState)
-  println("new player: " + newGame.player.playerLabel)
-  newGame.update(Action(Location(1, 0))) //X
-  println("state: " + newGame.state.gameState)
-  println("new player: " + newGame.player.playerLabel)
-  newGame.update(Action(Location(2, 2))) //O
-  println("state: " + newGame.state.gameState)
-  println("new player: " + newGame.player.playerLabel)
-  newGame.update(Action(Location(0,2))) //X
-  println("state: " + newGame.state.gameState)
-  println("new player: " + newGame.player.playerLabel)
-  newGame.update(Action(Location(1,2))) //O
-  println("state: " + newGame.state.gameState)
-  println("new player: " + newGame.player.playerLabel)
-  newGame.update(Action(Location(2,1))) //X
-  println("state: " + newGame.state.gameState)
-  println("new player: " + newGame.player.playerLabel)
-  newGame.update(Action(Location(0,1))) //O
-  println("state: " + newGame.state.gameState)
-  println("new player: " + newGame.player.playerLabel)
-  newGame.update(Action(Location(0,0))) //X
-  println("state: " + newGame.state.gameState)
-  println("new player: " + newGame.player.playerLabel)
+  println("******NEW GAME*********")
+  var g = new Game[Location] with UpdateTicTacToe with FieldTicTacToe {}
+  println("player: " + g.player.playerLabel)
+  g.updateGameState(Action(Location(1,1))) //X
+  println("field: " + g.field(1)(1))
+  println("state: " + g.state.gameState)
+  println("new player: " + g.player.playerLabel)
+  g.updateGameState(Action(Location(2, 0))) //O
+  println("state: " + g.state.gameState)
+  println("new player: " + g.player.playerLabel)
+  g.updateGameState(Action(Location(1, 0))) //X
+  println("state: " + g.state.gameState)
+  println("new player: " + g.player.playerLabel)
+  g.updateGameState(Action(Location(2, 2))) //O
+  println("state: " + g.state.gameState)
+  println("new player: " + g.player.playerLabel)
+  g.updateGameState(Action(Location(0,2))) //X
+  println("state: " + g.state.gameState)
+  println("new player: " + g.player.playerLabel)
+  g.updateGameState(Action(Location(1,2))) //O
+  println("state: " + g.state.gameState)
+  println("new player: " + g.player.playerLabel)
+  g.updateGameState(Action(Location(2,1))) //X
+  println("state: " + g.state.gameState)
+  println("new player: " + g.player.playerLabel)
+  g.updateGameState(Action(Location(0,1))) //O
+  println("state: " + g.state.gameState)
+  println("new player: " + g.player.playerLabel)
+  g.updateGameState(Action(Location(0,0))) //X
+  println("state: " + g.state.gameState)
+  println("new player: " + g.player.playerLabel)
 
-
-  var newGame2 = new TicTacToe {}
+  println("******NEW GAME*********")
+  var newGame2 = new Game[Location] with UpdateTicTacToe with FieldTicTacToe {}
   println("player: " + newGame2.player.playerLabel)
-  newGame2.update(Action(Location(1,1))) //X
+  newGame2.updateGameState(Action(Location(1,1))) //X
   println("field: " + newGame2.field(1)(1))
   println("state: " + newGame2.state.gameState)
   println("new player: " + newGame2.player.playerLabel)
-  newGame2.update(Action(Location(2, 1))) //O
+  newGame2.updateGameState(Action(Location(2, 1))) //O
   println("state: " + newGame2.state.gameState)
   println("new player: " + newGame2.player.playerLabel)
-  newGame2.update(Action(Location(0, 0))) //X
+  newGame2.updateGameState(Action(Location(0, 0))) //X
   println("state: " + newGame2.state.gameState)
   println("new player: " + newGame2.player.playerLabel)
-  newGame2.update(Action(Location(2, 0))) //O
+  newGame2.updateGameState(Action(Location(2, 0))) //O
   println("state: " + newGame2.state.gameState)
   println("new player: " + newGame2.player.playerLabel)
-  newGame2.update(Action(Location(2,2))) //X
+  newGame2.updateGameState(Action(Location(2,2))) //X
   println("state: " + newGame2.state.gameState)
   println("new player: " + newGame2.player.playerLabel)
 }
