023d50d772662af49e37f6af6affe2f8e7f9d4bb
Thomy Phan <thomyphan@yahoo.com>
2017-02-18 12:41:25 +0100

    some test users added

--- server/conf/evolutions/default/1.sql
+++ server/conf/evolutions/default/1.sql
@@ -12,5 +12,7 @@ create table "Users" (
 );
 
 Insert into "Users" VALUES (0, "Sabrina", "Friedl", "Sabrina", "sabrina_friedl@gmx.de", "test", 2000);
+Insert into "Users" VALUES (1, "Admin", "Istrator", "admin", "admin@istra.tor", "admin", 2000);
+Insert into "Users" VALUES (2, "Test", "Test", "test", "test@test.you", "test", 2000);
 
 # --- !Downs
